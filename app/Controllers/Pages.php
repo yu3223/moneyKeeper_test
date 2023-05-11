c75a5589daf75cfcf974799b362d9cec0fa0e960
沈于庭 <jenny910223@gmail.com>
2023-05-12 02:56:36 +0800

    完成註冊前端畫面，後端還沒連

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -11,19 +11,14 @@ class Pages extends Controller
         return view('welcome_message');
     }
 
-    public function view($page = 'home')
+    public function view($page = 'register')
     {
         if ( ! is_file(APPPATH.'/Views/pages/'.$page.'.php'))
         {
             // Whoops, we don't have a page for that!
             throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
         }
-
-        $data['title'] = ucfirst($page); // Capitalize the first letter
-
-        echo view('templates/header', $data);
-        echo view('pages/'.$page, $data);
-        echo view('templates/footer', $data);
+        echo view('pages/'.$page);
     }
     // public function view($page = 'home')
     // {
